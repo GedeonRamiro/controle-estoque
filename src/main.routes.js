b18152d936a0db398d04e12c70100db1e2861abd
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-04-07 15:17:04 -0300

    feat: create router /public-poducts

--- src/main.routes.js
+++ src/main.routes.js
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import CreateAccount from './pages/CreateAccount'
 import Login from "./pages/Login";
 import Products from './pages/Products'
+import PublicProduct from './pages/Products/PublicProduct'
 import CreateProduct from './pages/Products/CreateProduct'
 import Categoty from './pages/Categoty'
 import CreateCategory from './pages/Categoty/CreateCategory'
@@ -19,6 +20,7 @@ const MainRoutes = () => {
                 <Route index element={<ProtectedRoute> <Home /> </ProtectedRoute>} /> 
                 <Route path={'/login'} element={<Login />} />
                 <Route path={'/create-account'} element={<CreateAccount />} />
+                <Route path={'/public-poducts'} element={<PublicProduct />} />
                 <Route path={'/'} element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
                 <Route path={'/produtos'} element={<ProtectedRoute> <Products /> </ProtectedRoute>} />
                 <Route path={'/categorias'} element={<ProtectedRoute> <Categoty /> </ProtectedRoute>} />
---
+++ src/pages/Products/PublicProduct.tsx
@@ -0,0 +1,7 @@
+const PublicProduct = () => {
+    return (
+        <h1>produtos publico</h1>
+    )
+}
+
+export default PublicProduct
