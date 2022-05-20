05bf41064b93f4c715def8c82f0d4e8e3fb6e31c
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-20 16:03:22 -0300

    import tw-elements

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { ToastProvider } from 'react-toast-notifications';
-
+import 'tw-elements';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <ToastProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-      </ToastProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ToastProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ToastProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-
