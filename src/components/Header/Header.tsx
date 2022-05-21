d3d4ef5215d45044ee757f196a9630190dd60a38
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-21 18:47:37 -0300

    style: import component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,31 @@
-import { FaStoreAlt } from "react-icons/fa"
-import { MdMiscellaneousServices } from "react-icons/md"
-import { AiFillHome } from 'react-icons/ai'
-import { MdProductionQuantityLimits } from 'react-icons/md'
-import { BiCategory } from 'react-icons/bi'
-import { Link } from "react-router-dom"
-import { useAuth } from '../../context/auth'
+import { FaStoreAlt } from 'react-icons/fa';
+import { MdMiscellaneousServices } from 'react-icons/md';
+import { AiFillHome } from 'react-icons/ai';
+import { MdProductionQuantityLimits } from 'react-icons/md';
+import { BiCategory } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../context/auth';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 type ParamsId = {
-    id: string
-}
+    id: string;
+};
 
 type IProps = {
-    children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
+const Header = ({ children }: IProps) => {
+    const auth = useAuth();
 
-const Header = ( { children }: IProps ) => {
-
-    const  auth = useAuth()
+    console.log(auth.user.user_metadata.name);
 
     const openStore = () => {
-        window.open(`/public-poducts/${auth.user.id}`)
-    }
+        window.open(`/public-poducts/${auth.user.id}`);
+    };
 
     return (
-        <div className="h-screen drawer">
+        /*  <div className="h-screen drawer">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
             <div className="flex flex-col drawer-content">
                 <div className="w-full navbar bg-neutral">
@@ -115,8 +116,134 @@ const Header = ( { children }: IProps ) => {
                 </li>
                 </ul>
             </div>
-        </div>
-    )
-}
+        </div> */
 
-export default Header
+        <>
+            <nav className='relative flex flex-wrap items-center justify-between w-full py-4 text-gray-500 bg-gray-100 shadow-lg hover:text-gray-700 focus:text-gray-700 navbar navbar-expand-lg navbar-light'>
+                <div className='container mx-auto'>
+                    <div className='flex flex-wrap items-center justify-between w-full px-6 container-fluid'>
+                        <button
+                            className='
+                        navbar-toggler
+                        text-gray-500
+                        border-0
+                        hover:shadow-none hover:no-underline
+                        py-2
+                        px-2.5
+                        bg-transparent
+                        focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline
+                        '
+                            type='button'
+                            data-bs-toggle='collapse'
+                            data-bs-target='#navbarSupportedContent'
+                            aria-controls='navbarSupportedContent'
+                            aria-expanded='false'
+                            aria-label='Toggle navigation'
+                        >
+                            <svg
+                                aria-hidden='true'
+                                focusable='false'
+                                data-prefix='fas'
+                                data-icon='bars'
+                                className='w-6'
+                                role='img'
+                                xmlns='http://www.w3.org/2000/svg'
+                                viewBox='0 0 448 512'
+                            >
+                                <path
+                                    fill='currentColor'
+                                    d='M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z'
+                                ></path>
+                            </svg>
+                        </button>
+                        <div
+                            className='items-center flex-grow collapse navbar-collapse'
+                            id='navbarSupportedContent'
+                        >
+                            <ul className='flex flex-col pl-0 mr-auto navbar-nav list-style-none'>
+                                <li className='p-2 nav-item'>
+                                    <Link to={'/'}>
+                                        <a className='underline transition duration-300 ease-in-out decoration-transparent hover:decoration-inherit'>
+                                            Dashboard
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className='p-2 nav-item'>
+                                    <Link to={'/produtos'}>
+                                        <a className='underline transition duration-300 ease-in-out decoration-transparent hover:decoration-inherit'>
+                                            Produtos
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className='p-2 nav-item'>
+                                    <Link to={'/categorias'}>
+                                        <a className='underline transition duration-300 ease-in-out decoration-transparent hover:decoration-inherit'>
+                                            Categorias
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className='p-2 nav-item'>
+                                    <a
+                                        onClick={openStore}
+                                        className='underline transition duration-300 ease-in-out cursor-pointer decoration-transparent hover:decoration-inherit'
+                                    >
+                                        Minha Loja
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className='relative dropdown'>
+                            <a
+                                className='flex items-center dropdown-toggle hidden-arrow'
+                                href='#'
+                                id='dropdownMenuButton2'
+                                role='button'
+                                data-bs-toggle='dropdown'
+                                aria-expanded='false'
+                            >
+                                <p className='mr-2'>Olá, {auth.user.user_metadata.name}</p>
+
+                                <div className='flex justify-center space-x-2'>
+                                    <button
+                                        type='button'
+                                        className='flex items-center justify-center leading-normal text-white uppercase transition duration-150 ease-in-out bg-blue-600 rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg w-9 h-9'
+                                    >
+                                        <BsFillPersonFill />
+                                    </button>
+                                </div>
+                            </a>
+                            <ul
+                                className='absolute z-50 hidden float-left py-2 m-0 mt-1 text-base text-left list-none bg-white border-none rounded-lg shadow-lg dropdown-menu min-w-max bg-clip-padding'
+                                aria-labelledby='dropdownMenuButton2'
+                            >
+                                <li>
+                                    <Link to={'/configuracoes'}>
+                                        <a
+                                            className='block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100'
+                                            href='#'
+                                        >
+                                            Configurações
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <a
+                                        onClick={auth.logout}
+                                        className='block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100'
+                                        href='#'
+                                    >
+                                        Sair
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div className='container mx-auto'>{children}</div>
+        </>
+    );
+};
+
+export default Header;
