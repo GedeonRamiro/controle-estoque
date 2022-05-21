a10e85f80cfc63b5142144d9b1b98777340a850b
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-21 18:47:17 -0300

    feat: prettierrc

--- src/pages/Categoty/Categoty.tsx
+++ src/pages/Categoty/Categoty.tsx
@@ -59,71 +59,67 @@ const Categoty = () => {
 
     return (
         <Header>
-            <div className='container mx-auto'>
-                <div className='flex flex-col'>
+            <div className='flex flex-col'>
+                <div>
                     <Link to={'/adicionar-categoria'}>
                         <button className='my-10 btn btn-block sm:btn-wide btn-sm'>
                             Nova Categoria
                         </button>
                     </Link>
-                    <div className='w-full'>
-                        <div className='flex border-b border-gray-200 shadow'>
-                            <table className='w-full'>
-                                {categories && (
-                                    <thead className='bg-gray-50'>
-                                        <tr>
-                                            <th className='px-6 py-2 text-xs text-gray-500'>
-                                                Categorias
-                                            </th>
-                                            <th className='px-6 py-2 text-xs text-gray-500'>
-                                                Ação
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                )}
-                                {categories &&
-                                    categories.map((category) => (
-                                        <tbody
-                                            key={category.id}
-                                            className='text-center bg-white border'
-                                        >
-                                            <tr className='whitespace-nowrap'>
-                                                <td className='px-6 py-4'>
-                                                    <div className='text-sm font-semibold text-gray-900'>
-                                                        {category.name}
-                                                    </div>
-                                                </td>
-                                                <td className='px-6 py-4'>
-                                                    <Link
-                                                        to={'/adicionar-categoria'}
-                                                        state={category}
-                                                    >
-                                                        <button className='p-2 mx-1 text-white bg-blue-500 border-2 border-blue-600 rounded shadow-lg'>
-                                                            <FiEdit />
-                                                        </button>
-                                                    </Link>
-                                                    <button
-                                                        onClick={() => removeCategory(category.id)}
-                                                        className='p-2 mx-1 text-white bg-red-500 border-2 border-red-700 rounded shadow-lg'
-                                                    >
-                                                        <RiDeleteBin2Line />
+                </div>
+                <div className='w-full'>
+                    <div className='flex border-b border-gray-200 shadow'>
+                        <table className='w-full'>
+                            {categories && (
+                                <thead className='bg-gray-50'>
+                                    <tr>
+                                        <th className='px-6 py-2 text-xs text-gray-500'>
+                                            Categorias
+                                        </th>
+                                        <th className='px-6 py-2 text-xs text-gray-500'>Ação</th>
+                                    </tr>
+                                </thead>
+                            )}
+                            {categories &&
+                                categories.map((category) => (
+                                    <tbody
+                                        key={category.id}
+                                        className='text-center bg-white border'
+                                    >
+                                        <tr className='whitespace-nowrap'>
+                                            <td className='px-6 py-4'>
+                                                <div className='text-sm font-semibold text-gray-900'>
+                                                    {category.name}
+                                                </div>
+                                            </td>
+                                            <td className='px-6 py-4'>
+                                                <Link to={'/adicionar-categoria'} state={category}>
+                                                    <button className='p-2 mx-1 text-white bg-blue-500 border-2 border-blue-600 rounded shadow-lg'>
+                                                        <FiEdit />
                                                     </button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    ))}
-                                {categories?.length === 0 && loading && (
-                                    <div className='flex flex-col items-center'>
-                                        <div className='mb-10 text-lg font-semibold text-center'>
-                                            Lista vazia!
-                                        </div>
+                                                </Link>
+                                                <button
+                                                    onClick={() => removeCategory(category.id)}
+                                                    className='p-2 mx-1 text-white bg-red-500 border-2 border-red-700 rounded shadow-lg'
+                                                >
+                                                    <RiDeleteBin2Line />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                ))}
+                            {categories?.length === 0 && loading && (
+                                <div className='flex flex-col items-center'>
+                                    <div className='mb-10 text-lg font-semibold text-center'>
+                                        Lista vazia!
                                     </div>
-                                )}
-                            </table>
-                        </div>
+                                </div>
+                            )}
+                        </table>
                     </div>
                 </div>
             </div>
+
             {!loading && (
                 <div className='absolute inset-0 flex flex-col items-center justify-center'>
                     <button className='btn btn-sm btn-ghost loading'>loading</button>
