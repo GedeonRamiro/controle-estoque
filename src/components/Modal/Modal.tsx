e90407270bed7f58d55d5f9e42cb39d0d7982ca6
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-26 16:37:30 -0300

    feat: create component Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,28 +5,36 @@ type Props = {
     open: string | ReactNode;
     btnAction: string;
     children: ReactNode;
+    onClick: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Modal = ({ id, open, btnAction, children }: Props) => {
+const Modal = ({ id, open, btnAction, children, onClick }: Props) => {
+    console.log(Modal);
+
     return (
         <div>
-            <label htmlFor={`my-modal${id}`} className='cursor-pointer'>
-                {open}
-            </label>
+            {open}
             <input type='checkbox' id={`my-modal${id}`} className='modal-toggle' />
-            <div className=' modal'>
-                <div className='relative modal-box'>
+            {/*   <div className='flex items-center p-4 modal'> */}
+            <div className='flex items-center p-4 modal'>
+                <div className='px-20 py-4 bg-white rounded'>
                     {children}
-                    <div className='flex justify-center modal-action'>
-                        <a href={`#`}>
+                    <div className='flex justify-center '>
+                        <label
+                            className='inline-block px-6 py-2 mx-2 text-xs font-medium leading-tight text-blue-600 uppercase transition duration-150 ease-in-out border-2 border-blue-600 rounded cursor-pointer hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0'
+                            htmlFor={`my-modal${id}`}
+                        >
+                            Cancelar
+                        </label>
+
+                        <button onClick={onClick}>
                             <label
+                                className='inline-block  px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out cursor-pointer'
                                 htmlFor={`my-modal${id}`}
-                                className='btn btn-sm sm:btn btn-ghost sm:btn-ghost'
                             >
-                                Cancelar
+                                {btnAction}
                             </label>
-                        </a>
-                        <button className='btn btn-sm sm:btn'>{btnAction}</button>
+                        </button>
                     </div>
                 </div>
             </div>
