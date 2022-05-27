d4493e5fe386d7c3bc1a0369f9fc475a4becbbf6
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-27 16:41:16 -0300

    feat: create component Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,26 @@
 import { ReactNode } from 'react';
 
 type Props = {
-    id: string;
-    open: string | ReactNode;
-    btnAction: string;
+    id: string | number;
+    nameBtnOpen: string | ReactNode;
+    styleBtnOpen?: string;
+    btnAction?: string | ReactNode;
     children: ReactNode;
-    onClick: (ev: React.MouseEvent<HTMLButtonElement>) => void;
+    onClick?: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Modal = ({ id, open, btnAction, children, onClick }: Props) => {
+const Modal = ({ id, nameBtnOpen, styleBtnOpen, btnAction, children, onClick }: Props) => {
     console.log(Modal);
 
     return (
         <div>
-            {open}
+            <label htmlFor={`my-modal${id}`} className={styleBtnOpen}>
+                {nameBtnOpen}
+            </label>
             <input type='checkbox' id={`my-modal${id}`} className='modal-toggle' />
-            {/*   <div className='flex items-center p-4 modal'> */}
+
             <div className='flex items-center p-4 modal'>
-                <div className='px-20 py-4 bg-white rounded'>
+                <div className='relative modal-box'>
                     {children}
                     <div className='flex justify-center '>
                         <label
@@ -27,14 +30,16 @@ const Modal = ({ id, open, btnAction, children, onClick }: Props) => {
                             Cancelar
                         </label>
 
-                        <button onClick={onClick}>
-                            <label
-                                className='inline-block  px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out cursor-pointer'
-                                htmlFor={`my-modal${id}`}
-                            >
-                                {btnAction}
-                            </label>
-                        </button>
+                        {btnAction && (
+                            <button onClick={onClick}>
+                                <label
+                                    className='inline-block  px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out cursor-pointer'
+                                    htmlFor={`my-modal${id}`}
+                                >
+                                    {btnAction}
+                                </label>
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
