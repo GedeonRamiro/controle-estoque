81e93dbdb0ad61fcaf0e57b1a43cacb91ef2e6f5
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-06-23 15:15:33 -0300

    feat: create Component DoughnutComponent

---
+++ src/components/Chart/DoughnutComponent.tsx
@@ -0,0 +1,61 @@
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { generateColor } from 'utils/generateColor';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
+
+type DataChart =
+    | {
+          category: string;
+          price: number[];
+          quantidadDeProdutos: number;
+      }[]
+    | undefined;
+
+type Props = {
+    dataChart: DataChart;
+};
+
+const DoughnutComponent = ({ dataChart }: Props) => {
+    const dataDoughnut = {
+        labels: dataChart?.map((category: any) => category.category),
+
+        datasets: [
+            {
+                label: 'Categorias/Produtos',
+
+                data: dataChart?.map(
+                    (quantidadDeProdutos: any) => quantidadDeProdutos.quantidadDeProdutos
+                ),
+
+                backgroundColor: dataChart?.map((category: any) => generateColor(0.7)),
+                hoverOffset: 4,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+    return <Doughnut options={options} data={dataDoughnut} />;
+};
+
+export default DoughnutComponent;
